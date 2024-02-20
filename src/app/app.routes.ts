52d2d9f7583ba1c350f9acb8eb8ddf11dc0777a5
F80 <43803343+f80dev@users.noreply.github.com>
2024-02-20 19:51:27 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import {MainComponent} from "./main/main.component";
+import {LinKutComponent} from "./produits/lin-kut/lin-kut.component";
 
 export const routes: Routes = [
-  { path: '', component: MainComponent },
+  { path: '', component: MainComponent},
+  { path: 'linkut', component: LinKutComponent},
+  { path: 'mentions', component: LinKutComponent}
 ];
